a7bf48088f0fa257c1074273794c28767538e70a
Risky_11 <2114768901@qq.com>
2022-10-13 16:47:04 +0800

    js-learn

--- three_punch_combo.js
+++ three_punch_combo.js
@@ -1,12 +1,16 @@
-        $i = 0;
-        $('#start').click(function(){
-            $i++;
-            if($i >=6 ){
-                $('#start').hide();
-                $('#stop').show();
-            }
-        })
-        $('#stop').click(function(){
-            alert('这么作？今天别吃了！')
-            $(this).hide();
-        })
+$i = 0;
+$("#start").click(function () {
+  $i++;
+  if ($i >= 6) {
+    if (true) {
+      $("#start").hide();
+      $("#stop").show();
+    } else {
+      alert(i);
+    }
+  }
+});
+$("#stop").click(function () {
+  alert("这么作？今天别吃了！");
+  $(this).hide();
+});
